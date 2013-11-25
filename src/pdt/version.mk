fcfc23793b84dfdba059d8d5d21e22fda5dc172e
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:16:25 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/pdt/version.mk
+++ src/pdt/version.mk
@@ -1,3 +1,14 @@
-NAME = pdt_$(ROLLCOMPILER)
-VERSION = 3.18.1
-RELEASE = 0
+NAME               = pdt_$(ROLLCOMPILER)
+VERSION            = 3.18.1
+RELEASE            = 0
+PKGROOT            = /opt/pdt/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = pdt
+
+SOURCE_NAME        = pdtoolkit
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
