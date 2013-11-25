d35d73b835009884bd686aa13d5bc6bd77a52009
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:11:54 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/papi/version.mk
+++ src/papi/version.mk
@@ -1,3 +1,14 @@
-NAME	= papi_$(ROLLCOMPILER)
-VERSION = 5.0.1
-RELEASE = 0
+NAME               = papi_$(ROLLCOMPILER)
+VERSION            = 5.0.1
+RELEASE            = 0
+PKGROOT            = /opt/papi/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = papi
+
+SOURCE_NAME        = papi
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
