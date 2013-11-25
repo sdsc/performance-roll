b80b9a7aa3970528dafe9057ca49d414db657dad
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:21:19 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/tau/version.mk
+++ src/tau/version.mk
@@ -1,3 +1,14 @@
-NAME    = tau_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 2.22.p1
-RELEASE = 0
+NAME               = tau_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.22.p1
+RELEASE            = 0
+PKGROOT            = /opt/tau/$(ROLLCOMPILER)/$(ROLLMPI)
+
+SRC_SUBDIR         = tau
+
+SOURCE_NAME        = tau
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
