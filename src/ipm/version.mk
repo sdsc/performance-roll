72e0cc25dbb5bbd6fde5415f7f5eedf467cb3e07
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:02:09 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/ipm/version.mk
+++ src/ipm/version.mk
@@ -1,3 +1,15 @@
-NAME	= ipm_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION = 2.0.0
-RELEASE = 11
+NAME               = ipm_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 2.0.0
+RELEASE            = 11
+PKGROOT            = /opt/ipm/$(ROLLCOMPILER)/$(ROLLMPI)
+
+SRC_SUBDIR         = ipm
+
+SOURCE_NAME        = ipm
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+
