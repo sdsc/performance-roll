d45b575c874d65d8f1b9e146e9f59663746f1aab
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:06:54 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/mxml/version.mk
+++ src/mxml/version.mk
@@ -1,3 +1,14 @@
-NAME = mxml_$(ROLLCOMPILER)
-VERSION = 2.7
-RELEASE = 11
+NAME               = mxml_$(ROLLCOMPILER)
+VERSION            = 2.7
+RELEASE            = 11
+PKGROOT            = /opt/mxml/$(ROLLCOMPILER)
+
+SRC_SUBDIR         = mxml
+
+SOURCE_NAME        = mxml
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
