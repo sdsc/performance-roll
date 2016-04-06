a155d9e459ceb3c775dcf5a2e8228ad7dc06c3b4
Jerry Greenberg <jpg@comet-01-65.local>
2016-04-06 11:16:58 -0700

    APS-301: have tau get it's own bfd library

--- src/tau/version.mk
+++ src/tau/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-tau_$(COMPILERNAME)_$(MPINAME)
 VERSION        = 2.23
-RELEASE        = 3
+RELEASE        = 4
 PKGROOT        = /opt/tau/$(COMPILERNAME)/$(MPINAME)
 
 SRC_SUBDIR     = tau
